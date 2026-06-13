6650f1f98d790839986cc026b6eff7465b37fae9
Felipe Pereira Limeira De Souza <felipe.p.souza@edu.senai.br>
2026-06-13 00:28:35 +0000

    Licao 11: Tarefa 4: Verificar com testes concluida

--- wikipedia/test/model_test.dart
+++ wikipedia/test/model_test.dart
@@ -6,10 +6,9 @@ import 'package:wikipedia/src/model/article.dart';
 import 'package:wikipedia/src/model/search_results.dart';
 import 'package:wikipedia/src/model/summary.dart';
 
-// Mudamos aqui para incluir wikipedia/ no início dos caminhos:
-const String dartLangSummaryJson = 'wikipedia/test/test_data/dart_lang_summary.json';
-const String catExtractJson = 'wikipedia/test/test_data/cat_extract.json';
-const String openSearchResponse = 'wikipedia/test/test_data/open_search_response.json';
+const String dartLangSummaryJson = './test/test_data/dart_lang_summary.json';
+const String catExtractJson = './test/test_data/cat_extract.json';
+const String openSearchResponse = './test/test_data/open_search_response.json';
 
 void main() {
   group('deserialize example JSON responses from wikipedia API', () {
