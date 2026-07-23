e30b2784be031b4b52d5ec8c15b46521dfe9ecab
artowal <arthur.w.oliveira@edu.senai.br>
2026-07-22 23:35:27 -0300

    Update Hexa2030.dart

--- command_runner/lib/Hexa2030.dart
+++ command_runner/lib/Hexa2030.dart
@@ -19,7 +19,91 @@ class Selecao {
 
   int get saldoGols => golsPro - golsSofridos;
 
+  
+
   Map<String, int> confrontosDiretos = {};
 }
 
 // Parte 1 aluno 1 finalizada
+
+// Aluno 3 modelando os criterios de desempate
+
+class Partida {
+  final Selecao timeA;
+  final Selecao timeB;
+  final int golsA;
+  final int golsB;
+
+  Partida(this.timeA, this.timeB, this.golsA, this.golsB);
+}
+
+class Classificacao {
+  static void ordenarTabela(
+      List<Selecao> selecoes,
+      List<Partida> partidas,
+      ) {
+
+    selecoes.sort((a, b) {
+
+            if (a.pontos != b.pontos) {
+        return b.pontos.compareTo(a.pontos);
+      }
+
+            if (a.saldoGols != b.saldoGols) {
+        return b.saldoGols.compareTo(a.saldoGols);
+      }
+      
+            if (a.golsPro != b.golsPro) {
+        return b.golsPro.compareTo(a.golsPro);
+      }
+
+            for (var jogo in partidas) {
+
+        bool confronto =
+            (jogo.timeA == a && jogo.timeB == b) ||
+            (jogo.timeA == b && jogo.timeB == a);
+
+        if (confronto) {
+
+          if (jogo.timeA == a) {
+            if (jogo.golsA > jogo.golsB) return -1;
+            if (jogo.golsA < jogo.golsB) return 1;
+          } else {
+            if (jogo.golsB > jogo.golsA) return -1;
+            if (jogo.golsB < jogo.golsA) return 1;
+          }
+
+          break;
+        }
+      }
+
+      int fairPlayA = a.cartoesAmarelos + (a.cartoesVermelhos * 3);
+      int fairPlayB = b.cartoesAmarelos + (b.cartoesVermelhos * 3);
+
+      if (fairPlayA != fairPlayB) {
+        return fairPlayA.compareTo(fairPlayB);
+      }
+
+      return a.rankingFifa.compareTo(b.rankingFifa);
+    });
+  }
+
+  static void mostrarTabela(List<Selecao> selecoes) {
+    print("==============================================");
+    print("Pos | Seleção         | Pts | SG | GP | FIFA");
+    print("----------------------------------------------");
+
+    for (int i = 0; i < selecoes.length; i++) {
+      var s = selecoes[i];
+
+      print(
+          "${i + 1}º  | ${s.nome.padRight(15)} | "
+          "${s.pontos.toString().padLeft(3)} | "
+          "${s.saldoGols.toString().padLeft(2)} | "
+          "${s.golsPro.toString().padLeft(2)} | "
+          "${s.rankingFifa}º");
+    }
+  }
+}
+
+// Parte 3 aluno 3 finalizada
